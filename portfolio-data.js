b97601583a0d2d0710ce754c0902d1b0306fab4e
Jose Hernando Lopez <kota012011@gmail.com>
2026-09-02 14:42:22 -0500

    Update contact information in portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -6,9 +6,9 @@ const portfolioData = {
   personalInfo: {
     nombre: "Ing. Jose H. Lopez",
     titulo_profesional: "Data Architect & Engineer",
-    correo: "contacto@tuempresa.com",
+    correo: "jose.lopez@datafreengineer.com",
     calendario_url: "https://wa.link/6m83en",
-    whatsApp: "wa.link/h5y7xk",
+    whatsApp: "https://wa.link/cqvkse",
     linkedin_url:
       "https://www.linkedin.com/in/jose-hernando-l%C3%B3pez-trejos-599b49335/",
     github_url: "https://github.com/kotaErn650",
